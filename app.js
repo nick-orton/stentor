8bf9458a69f67ac4c43f83e4c0fd28cab45a65f6
Nick Orton <nick@orton.biz>
2012-05-05 16:29:15 +0000

    finds if caller is in group

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ var tropowebapi = require('tropo-webapi');
 var http = require('http');
 var Db = require('mongodb').Db;
 var Server = require('mongodb').Server;
+var _ = require('underscore');
 
 var client = new Db('muppetsdb', new Server('127.0.0.1', 27017));
-
-
-
 
 var server = http.createServer(function (request, response) {
 
@@ -23,12 +21,23 @@ var server = http.createServer(function (request, response) {
 
     client.open(function(err, pClient) {
       client.collection('muppets', function(err, collection) {
-        collection.find().toArray(function(err, numbers) {
+        collection.find().toArray(function(err, members) {
           client.close(); //we don't need no open connections
 
-          //do stuff with the numbers here
-          tropo.say("Hello, " + caller + ":" + message + numbers);
+          var nums = _.map(members, function(e){
+            return e.number
+          });
+
+          if(_.include(nums, caller)) {
+            tropo.say("matches");
+          } else {
+            tropo.say("not in the group");
+          }
+
           tropo.hangup();
+
+          //do stuff with the members here
+          //tropo.say("Hello, " + caller + ":" + message + nums);
 
           response.writeHead(200, {'Content-Type': 'application/json'});
 
